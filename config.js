626984c7c1f1e576d9ffd2621147fb694ff5a27c
bangokun <108464869+bangokun@users.noreply.github.com>
2022-08-26 20:15:50 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 let fs = require('fs')
 let chalk = require('chalk')
 global.owner = [
-  ['84585961987', 'Chonix', true],
+  ['6285738639969', 'Chonix', true],
   // [number, dia creator/owner?, dia developer?]
   JSON.parse(fs.readFileSync('./src/owner.json'))
 ] // Put your number here
@@ -25,15 +25,15 @@ global.APIKeys = { // APIKey Here
 //=========== Sticker WM =============//
 const stick = JSON.parse(fs.readFileSync("lib/exif.json"))
 if (stick.spackname == '' || stick.sauthor == '') {
-  var sticker_name = 'Made with WeA Bot'
-  var sticker_author = '𝖻𝗂𝗍.𝗅𝗒/𝖠𝖼𝖾𝗅𝗅𝖢𝗈𝗆𝖾𝗅'
+  var sticker_name = 'Made with Chonix Bot'
+  var sticker_author = 'https://s.id/1fNe4'
 } else {
   var sticker_name = stick.spackname
   var sticker_author = stick.sauthor
 }
 const weem = JSON.parse(fs.readFileSync("lib/wm.json"))
 if (weem.watermark == '') {
-  var hias_watermark = 'ᴡᴇᴀ ʙᴏᴛ ʙʏ ʀᴀsᴇʟ ⌤'
+  var hias_watermark = 'Chonix'
 } else {
   var hias_watermark = weem.watermark
 } 
